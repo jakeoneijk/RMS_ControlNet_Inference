18b605a0f4a915e87fdaf9855f0098282fb5c38e
jakeoneijk <jakeoneijk@gmail.com>
2024-05-29 12:51:55 +0900

    update

--- Test.py
+++ Test.py
@@ -13,7 +13,16 @@ audio:torch.Tensor = torch.from_numpy(audio).unsqueeze(0) #[1,time]
 rms:torch.Tensor = torch.from_numpy(RMS.get_rms_fit_to_audio_ldm_mel(audio=audio)) #[1, time/hop]
 #discretized_rms:torch.Tensor = RMS.get_discretized_rms(rms, 128)
 
-audio_ldm_controlnet = AudioLDMControlNet()
+audio_ldm_controlnet = AudioLDMControlNet(
+    config_yaml_path = './ModelWeight/audioldm_original.yaml',
+    reload_from_ckpt = './ModelWeight/audioldm-s-full.ckpt',
+    control_net_pretrained_path = './ModelWeight/ControlNetstep300000.pth',
+    clap_for_condition_pretrained_path='./ModelWeight/clap_htsat_tiny.pt',
+    clap_for_cossim_pretrained_path = './ModelWeight/clap_music_speech_audioset_epoch_15_esc_89.98.pt',
+    vae_pretrained_path = './ModelWeight/vae_mel_16k_64bins.ckpt',
+    vocoder_pretrained_path='./ModelWeight/hifigan_16k_64bins',
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+)
 generated_audio_by_text:np.ndarray = audio_ldm_controlnet.generate(
     text_prompt='people fart',
     rms=rms
